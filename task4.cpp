d2d84f188b29446aaf93a5a96da6202bc4e9f19d
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-17 16:02:43 +0300

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int i, n, min = 0, max = 0;
+	cout << "Введите размерность массиа: ";
+	cin >> n;
+
+	int *arr = new int[n];
+
+	for (i = 0; i < n; i++)
+	{
+		cout << "Введите элемент: ";
+		cin >> arr[i];
+		min = arr[i] < arr[min] ? i : min;
+		max = arr[i] > arr[max] ? i : max;
+	}
+
+	if (min < max)
+	{ 
+		for (i = min + 1; i < max; i++)
+		{
+			arr[i] = 0;
+		}
+	}
+	else for (i = max + 1; i < min; i++)
+	{
+		arr[i] = 0;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Элемент: " << arr[i] << endl;
+	}
+
+	system("pause");
+	return 0;
+}
