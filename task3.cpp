81d19156cdeb0431eaf5991280c1e4c73a8dc7c2
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-17 15:58:25 +0300

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int i, n, lod;
+	cout << "Введите размерность массива: ";
+	cin >> n;
+	lod = 0;
+
+	int *a = new int[n];
+
+	for (i = 0; i < n; i++)
+	{
+		cout << "Введите элемент массива: ";
+		cin >> a[i];
+		if (a[i] % 2 == 1) lod = a[i];
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		if (a[i] % 2 == 1)
+		{ 
+			a[i] = a[i] + lod;
+		}
+		cout << "Элемент массива: " << a[i] << endl;
+	}
+
+	system("pause");
+	return 0;
+}
