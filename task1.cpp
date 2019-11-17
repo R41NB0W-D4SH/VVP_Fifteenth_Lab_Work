e1bbd9443e5119a9b586dafc79fe7a8fb4f068e5
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-17 15:39:10 +0300

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int i, n, temp;
+	cout << "Введите размерность массивов: ";
+	cin >> n;
+
+	int *a = new int[n];
+	int *b = new int[n];
+
+	for (i = 0; i < n; i++)
+	{
+		cout << "Введите элемент массива A: ";
+		cin >> a[i];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Введите элемент массива B: ";
+		cin >> b[i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		temp = a[i];
+		a[i] = b[i];
+		b[i] = temp;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Массив A: " << a[i] << endl;
+	}
+	cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Массив B: " << b[i] << endl;
+	}
+
+	system("pause");
+	return 0;
+}
